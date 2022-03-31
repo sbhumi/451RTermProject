2a9ef6065ed0321ac69afc6cf64a59623a246399
Christian Chapman <cachapman2019@gmail.com>
2022-03-30 20:23:16 -0500

    pushing react files with home page, fixed prior issues

---
+++ src/components/SiteFooter.js
@@ -0,0 +1,12 @@
+import '../styleSheets/SiteFooter.css';
+
+function SiteFooter() {
+    return (
+        
+        <div className="footerContainer">
+            <h6 className="footerText">Courtesy of Team Error 404</h6>
+        </div>
+    );
+}
+
+export default SiteFooter;
